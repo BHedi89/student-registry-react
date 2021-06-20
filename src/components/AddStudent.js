694201709a656b1f675c58b7c99340dd70b159c6
BHedi89 <hedi.besenyei89@gmail.com>
2021-06-20 15:50:26 +0200

    set up add new student page

--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -1,36 +1,114 @@
 import { Component } from "react";
-import Header from './Header'
+import Header from "./Header";
+import { Form, Button } from "react-bootstrap";
 
 class AddStudent extends Component {
-    render() {
-        return (
-            <div className='container'>
-                <Header title='Új hallgató hozzáadása' buttonTitle='Vissza' buttonLink='/studentList' />
-                <div className="form-group">
-                    <label htmlFor="inputName">Név</label>
-                    <input type="text" className="form-control" id="inputName" placeholder="Név"/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="inputEmail">E-mail cím</label>
-                    <input type="email" className="form-control" id="inputEmail" placeholder="E-mail cím"/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="inputAge">Életkor</label>
-                    <input type="number" className="form-control" id="inputAge" placeholder="Életkor"/>
-                </div>
-                <div className="form-group">
-                    <label className="mr-2" htmlFor="inputGender">Nem</label>
-                    <select className="custom-select my-1 mr-sm-2" id="inputGender">
-                        <option value="FEMALE">Nő</option>
-                        <option value="MALE">Férfi</option>
-                        <option value="2">Egyéb</option>
-                        <option value="3">Nem szeretném megadni</option>
-                    </select>
-                </div>
-                <button className="btn btn-primary">Mentés</button>
-            </div>
-        )
+  constructor(props) {
+    super(props);
+    this.state = {
+      validated: false,
+    };
+  }
+
+  addNewStudent = () => {
+      
+  }
+
+  handleSubmit = (e) => {
+    const form = e.currentTarget;
+    if (form.checkValidity() === true) {
+      this.setState({
+        validated: true,
+      });
     }
+  };
+
+  inputChangeHandler(changeObject) {
+    this.setState(changeObject);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header
+          title="Új hallgató hozzáadása"
+          buttonTitle="Vissza"
+          buttonLink="/studentList"
+        />
+        <Form noValidate validated={this.state.validated}>
+          <Form.Group controlId="validationName">
+            <Form.Label>Név</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Név"
+              required
+              onChange={(e) => {
+                this.inputChangeHandler({ name: e.target.value });
+              }}
+            />
+            <Form.Control.Feedback type="invalid">
+              Név megadása kötelező
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group controlId="validationEmail">
+            <Form.Label>E-mail cím</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="E-mail cím"
+              required
+              onChange={(e) => {
+                this.inputChangeHandler({ email: e.target.value });
+              }}
+            />
+            <Form.Control.Feedback type="invalid">
+              E-mail cím megadása kötelező
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group controlId="validationAge">
+            <Form.Label>Életkor</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Életkor"
+              required
+              onChange={(e) => {
+                this.inputChangeHandler({ age: e.target.value });
+              }}
+            />
+            <Form.Control.Feedback type="invalid">
+              Életkor megadása kötelező
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group controlId="validationGender">
+            <Form.Label className="mr-2">Nem</Form.Label>
+            <Form.Control
+              required
+              as="select"
+              className="custom-select my-1 mr-sm-2"
+              onChange={(e) => {
+                this.inputChangeHandler({ gender: e.target.value });
+              }}
+            >
+              <option value="">Válassz...</option>
+              <option value="FEMALE">Nő</option>
+              <option value="MALE">Férfi</option>
+              <option value="2">Egyéb</option>
+              <option value="3">Nem szeretném megadni</option>
+            </Form.Control>
+            <Form.Control.Feedback type="invalid">
+              Nem megadása kötelező
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Form>
+        <Button
+          className="btn btn-primary"
+          onClick={this.handleSubmit}
+          type="button"
+        >
+          Mentés
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default AddStudent;
