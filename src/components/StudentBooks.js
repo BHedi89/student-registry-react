f0691a345b963c1bb1e4988afdfdcc83bc3bb53d
BHedi89 <hedi.besenyei89@gmail.com>
2021-07-06 14:08:28 +0200

    add table to the StudentBooks component

--- src/components/StudentBooks.js
+++ src/components/StudentBooks.js
@@ -1,35 +1,61 @@
 import { Component } from "react";
 import Header from "./Header";
 import NewBookForm from "./NewBookForm";
+import { Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 class StudentBooks extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            showForm: false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      showForm: false,
+    };
+  }
 
-    toggleForm(){
-        this.setState({
-            showForm: !this.state.showForm
-        })
-        console.log(this.state);
-    }
+  toggleForm() {
+    this.setState({
+      showForm: !this.state.showForm,
+    });
+    console.log(this.state);
+  }
 
-    render() {
-        return(
-            <div className="container">
-                <Header 
-                    buttonLink="/studentList"
-                    buttonTitle="Vissza"
-                    title={`${this.props.location.state.name} könyvei`}
-                />
-                <button className="btn btn-primary mb-3" onClick={() => this.toggleForm()}>Új könyv hozzáadása</button>
-                {this.state.showForm ? <NewBookForm /> : null}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <Header
+          buttonLink="/studentList"
+          buttonTitle="Vissza"
+          title={`${this.props.location.state.name} könyvei`}
+        />
+        <button
+          className="btn btn-primary mb-3"
+          onClick={() => this.toggleForm()}
+        >
+          Új könyv hozzáadása
+        </button>
+        {this.state.showForm ? <NewBookForm /> : null}
+
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Szerző</th>
+              <th>Cím</th>
+              <th>Alcím</th>
+              <th>Weboldal</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td><Link>xy</Link></td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
 
 export default StudentBooks;
