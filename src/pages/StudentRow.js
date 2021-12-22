094c00597cc8311c434916cd525e9167b1a4e4da
BHedi89 <hedi.besenyei89@gmail.com>
2021-12-22 10:04:12 +0100

    fix the re-render bug in StudentRow component

--- src/pages/StudentRow.js
+++ src/pages/StudentRow.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import ModifyStudentModal from "../modals/ModifyStudentModal";
 import { Modal } from "react-bootstrap";
 import DeleteStudentModal from "../modals/DeleteStudentModal";
@@ -6,113 +6,103 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
 import LinkComponent from "../UI/LinkComponent";
+import { useState } from 'react';
 
-class StudentRow extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: null,
-      studentId: null,
-    };
-  }
+//??????????
+const StudentRow = props => {
+  const [show, setShow] = useState(false);
+  const [studentId, setStudentId] = useState(null);
 
-  handleCloseModal = (id) => {
-    this.setState({
-      show: id,
-    });
+  const handleCloseModal = (id) => {
+    setShow(id);
   };
 
-  handleShowModal(studentId, modalId) {
-    this.setState({
-      studentId: studentId,
-      show: modalId,
-    });
+  function handleShowModal(studentId, modalId) {
+    setStudentId(studentId);
+    setShow(modalId);
   }
 
-  render() {
-    return (
-      <Fragment>
-        {this.props.students.map((student) => {
-          let booksNum = 0;
-          for (const book in student.books) {
-            booksNum += 1;
-          }
-          return (
-            <tr key={student.id}>
-              <td>{student.name}</td>
-              <td>{student.email}</td>
-              <td>{student.age}</td>
-              <td>{student.gender === "FEMALE" ? "nő" : "férfi"}</td>
-              <td>
-                {booksNum === 0 ? (
-                  <LinkComponent
-                    to={{
-                      pathname: `/studentBooks/${student.id}`,
-                      state: student,
-                    }}
-                    linkText="Nincs"
-                  />
-                ) : (
-                  <LinkComponent
-                    to={{
-                      pathname: `/studentBooks/${student.id}`,
-                      state: student,
-                    }}
-                    linkText={booksNum}
-                  />
-                )}
-              </td>
-              <td>
-                <Modal
-                  animation={false}
-                  show={
-                    this.state.show === "modal1" &&
-                    this.state.studentId === student.id
-                  }
-                  onHide={this.handleCloseModal}
-                >
-                  <Modal.Header closeButton>
-                    <Modal.Title>Adatok módosítása</Modal.Title>
-                  </Modal.Header>
-                  <Modal.Body>
-                    <ModifyStudentModal
-                      students={student}
-                      closeModal={this.handleCloseModal}
-                      onStudentUpdate={this.props.onStudentUpdate}
-                    />
-                  </Modal.Body>
-                </Modal>
-                <FontAwesomeIcon
-                  className="mr-4"
-                  icon={faPen}
-                  type="button"
-                  onClick={() => this.handleShowModal(student.id, "modal1")}
+  return (
+    <Fragment>
+      {props.students.map((student) => {
+        let booksNum = 0;
+        for (const book in student.books) {
+          booksNum += 1;
+        }
+        return (
+          <tr key={student.id}>
+            <td>{student.name}</td>
+            <td>{student.email}</td>
+            <td>{student.age}</td>
+            <td>{student.gender === "FEMALE" ? "nő" : "férfi"}</td>
+            <td>
+              {booksNum === 0 ? (
+                <LinkComponent
+                  to={{
+                    pathname: `/studentBooks/${student.id}`,
+                    state: student,
+                  }}
+                  linkText="Nincs"
                 />
-                <Modal
-                  animation={false}
-                  show={
-                    this.state.show === "modal2" &&
-                    this.state.studentId === student.id
-                  }
-                >
-                  <DeleteStudentModal
+              ) : (
+                <LinkComponent
+                  to={{
+                    pathname: `/studentBooks/${student.id}`,
+                    state: student,
+                  }}
+                  linkText={booksNum}
+                />
+              )}
+            </td>
+            <td>
+              <Modal
+                animation={false}
+                show={
+                  show === "modal1" &&
+                  studentId === student.id
+                }
+                onHide={handleCloseModal}
+              >
+                <Modal.Header closeButton>
+                  <Modal.Title>Adatok módosítása</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <ModifyStudentModal
                     students={student}
-                    closeModal={this.handleCloseModal}
-                    onUpdateAfterDelete={this.props.onUpdateAfterDelete}
+                    closeModal={handleCloseModal}
+                    onStudentUpdate={props.onStudentUpdate}
                   />
-                </Modal>
-                <FontAwesomeIcon
-                  icon={faTrash}
-                  type="button"
-                  onClick={() => this.handleShowModal(student.id, "modal2")}
+                </Modal.Body>
+              </Modal>
+              <FontAwesomeIcon
+                className="mr-4"
+                icon={faPen}
+                type="button"
+                onClick={() => handleShowModal(student.id, "modal1")}
+              />
+              <Modal
+                animation={false}
+                show={
+                  show === "modal2" &&
+                  studentId === student.id
+                }
+              >
+                <DeleteStudentModal
+                  students={student}
+                  closeModal={handleCloseModal}
+                  onUpdateAfterDelete={props.onUpdateAfterDelete}
                 />
-              </td>
-            </tr>
-          );
-        })}
-      </Fragment>
-    );
-  }
+              </Modal>
+              <FontAwesomeIcon
+                icon={faTrash}
+                type="button"
+                onClick={() => handleShowModal(student.id, "modal2")}
+              />
+            </td>
+          </tr>
+        );
+      })}
+    </Fragment>
+  );
 }
-
 export default StudentRow;
